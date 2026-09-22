0d970254a211d61a7ad7d1d836b527058db0f218
Damar <andreasdamarjati1994@gmail.com>
2026-09-22 09:23:26 +0700

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,41 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import "./index.css";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import BlogDetail from "./pages/BlogDetail";
+import CreateBlog from "./pages/CreateBlog";
+import { authLoader } from "./loaders/auth";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/blogs/:objectId",
+    element: <BlogDetail />,
+  },
+  {
+    path: "/write",
+    element: <CreateBlog />,
+    loader: authLoader,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
